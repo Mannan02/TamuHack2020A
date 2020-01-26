36e81445b76725c4ebdf282794dee0d6c40abdaf
Mannan <mannanmendiratta99@gmail.com>
2020-01-25 23:27:52 -0600

    Idk

--- util.js
+++ util.js
@@ -1,22 +1,22 @@
 const insulter = require('insult');
 const shakespereInsulter = require('shakespeare-insult1.1.0')
-var googleapis = require('googleapis');
-
+var request = require('request');
+var json = require('json')
 API_KEY = process.env.API_KEY;
 DISCOVERY_URL = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1';
-
+url = ('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze' +
+    '?key=' + API_KEY)
+data_dict = {
+    'comment': { 'text': 'what kind of idiot name is foo?' },
+    'languages': ['en'],
+    'requestedAttributes': { 'TOXICITY': {} }
+}
 exports.insultMe = () => insulter.Insult();
 
 exports.shakespereInsult = () => shakespereInsulter.random();
 
-exports.isInsult = comment => googleapis.discoverAPI(DISCOVERY_URL, (err, client) => {
-    if (err) throw err;
-    var analyzeRequest = {
-        comment: { text: comment },
-        requestedAttributes: { 'INSULT': {} }
-    };
-    client.comments.analyze({ key: API_KEY, resource: analyzeRequest }, (err, response) => {
-        if (err) throw err;
-        console.log(JSON.stringify(response, null, 2));
-    });
-});
+exports.isInsult = comment => {
+    response = requests.post(url = url, data = json.dumps(data_dict))
+    response_dict = json.loads(response.content)
+    console.log(json.dumps(response_dict, indent = 2))
+}
