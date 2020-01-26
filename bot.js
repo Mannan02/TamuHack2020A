9742a054994e902b517682772a99815431e844b5
Mannan <mannanmendiratta99@gmail.com>
2020-01-25 19:33:01 -0600

    insultme

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const insulter = require('insult');
 const util = require('./util.js')
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -28,7 +29,7 @@ client.on('message', msg => {
                     }
                     break;
                 case 'insultMe':
-                    msg.reply(util.insultMe())
+                    msg.reply(insulter.Insult())
                     break;
                 default:
                     msg.reply('?');
