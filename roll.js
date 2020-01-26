a6dff9cd358a5f488245c5da9568898233f6383b
VainName <cobalmeansbusiness@gmail.com>
2020-01-25 18:46:02 -0800

    added roll.js

---
+++ roll.js
@@ -0,0 +1,30 @@
+function Roll(args) {
+    var mods;
+    var randRoll;
+    var score;
+
+    try {
+        if (args.length <= 1) {
+            throw e;
+        }
+        else {
+            const dieRoll = params.split('d');
+            if (dieroll.length != 2) {
+                throw e;
+            }
+            else {
+                var qty = dieRoll[0];
+                var die = dieRoll[1];
+            }
+        }
+    }
+    catch (e) {
+        score.toString("FAILED >:(");
+    }
+    var randRoll = Math.floor((Math.random() * die ) + 1);
+    
+    var mods = 0;
+
+    var score = ((qty * die) + mods);
+    return(score);
+}
